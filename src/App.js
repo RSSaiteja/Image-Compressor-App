d733b49b32fdbdd4fcae5a453c0a87c986dce36e
RSSaiteja <91172508+RSSaiteja@users.noreply.github.com>
2022-02-15 00:27:18 +0530

    Removed Listgroup

--- src/App.js
+++ src/App.js
@@ -3,19 +3,6 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Instructions from "./components/Instructions"
 import Compressor from "./components/Compressor";
-// import Extract from "./components/Extract"
-// import Tesseract from 'tesseract.js';
-
-
-// Tesseract.recognize(
-//   'https://tesseract.projectnaptha.com/img/eng_bw.png',
-//   'eng',
-//   { logger: m => console.log(m) }
-// ).then(({ data: { text } }) => {
-//   console.log(text);
-// })
-
-
 
 
 function App() {
@@ -26,7 +13,7 @@ function App() {
       <Navbar title="Image - Compressor" />
       <Instructions />
       <Compressor/>
-      {/* <Extract/> */}
+      
     </>
   );
 }
